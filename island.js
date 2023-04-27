707d254a27e5bdc1dd1c911f7003570c116fcc21
James Yu Cao <caoyu19920414@gmail.com>
2023-04-27 12:33:34 -0400

    finished

--- island.js
+++ island.js
@@ -1,20 +1,60 @@
-function getNeighbors(row, col, graph) {
-
-  // Check top
-
-  // Check bottom
-
-  // Check left
-
-  // Check right
-
-  // Return neighbors
-
-  // Your code here
+function getNeighbors(row, col, matrix) {
+  let res = [];
+  if (row >= 1) {
+    if (matrix[row - 1][col] === 1) {
+      res.push([row - 1, col]);
+    }
+  }
+  if (row <= matrix.length - 2) {
+    if (matrix[row + 1][col] === 1) {
+      res.push([row + 1, col]);
+    }
+  }
+  if (col >= 1) {
+    if (matrix[row][col - 1] === 1) {
+      res.push([row, col - 1]);
+    }
+  }
+  if (col <= matrix[0].length - 2) {
+    if (matrix[row][col + 1] === 1) {
+      res.push([row, col + 1]);
+    }
+  }
+  return res;
 }
 
 
-function islandSize(row, col, graph) {
+function islandSize(row, col, matrix) {
+
+  // Not an island
+  if (matrix[row][col] === 0) {
+    return 0;
+  }
+  // Data Structure for BFS traversal
+  let queue = [];
+  let visited = new Set();
+  let area = 0;
+  //
+  function convert2DTo1D(row, col) {
+    return row * matrix[0].length + col;
+  }
+  // Initialize the traversal
+  queue.push([row, col]);
+  visited.add(convert2DTo1D(row, col));
+  while (queue.length > 0) {
+    let [r, c] = queue.shift();
+    area++;
+    let neighbors = getNeighbors(r, c, matrix);
+    for (let node of neighbors) {
+      let converted = convert2DTo1D(...node);
+      if (!visited.has(converted)) {
+        queue.push(node);
+        visited.add(converted);
+      }
+    }
+  }
+  return area;
+
 
   // Create a visited set to store visited nodes
 
