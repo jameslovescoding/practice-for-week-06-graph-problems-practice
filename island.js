c34386d8d7310edd900f3b13ceee0c0f81ec94e3
James Yu Cao <caoyu19920414@gmail.com>
2023-04-27 12:35:47 -0400

    finish

--- island.js
+++ island.js
@@ -54,30 +54,6 @@ function islandSize(row, col, matrix) {
     }
   }
   return area;
-
-
-  // Create a visited set to store visited nodes
-
-  // Create a stack, put the starting node in the stack
-
-  // Put the stringified starting node in visited
-
-  // Initialize size to 0
-
-  // While the stack is not empty,
-
-    // Pop the first node
-
-    // DO THE THING (increment size by 1)
-
-    // Then push all the UNVISITED neighbors on top of the stack
-    // and mark them as visited
-    // HINT: This is what your helper function `getNeighbors` is for
-    // HINT: Remember, you're storing your visited nodes as strings!
-
-  // return size
-
-  // Your code here
 }
 
 module.exports = [getNeighbors, islandSize];
